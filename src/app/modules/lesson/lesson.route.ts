536c14e5974a383793a26acdf849f01c2c4c45a7
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2024-11-26 17:06:00 +0600

    update lesson route auth

--- src/app/modules/lesson/lesson.route.ts
+++ src/app/modules/lesson/lesson.route.ts
@@ -2,11 +2,14 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { LessonControllers } from "./lesson.controller";
 import { LessonValidations } from "./lesson.validation";
+import auth from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.constant";
 
 const router = express.Router();
 
 router.post(
   "/create-lesson",
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(LessonValidations.createLessonValidationSchema),
   LessonControllers.createLesson,
 );
@@ -16,10 +19,15 @@ router.get("/", LessonControllers.getAllLessons);
 
 router.patch(
   "/update-lesson/:lessonId",
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(LessonValidations.updateLessonValidationSchema),
   LessonControllers.updateLesson,
 );
 
-router.delete("/delete-lesson/:lessonId", LessonControllers.deleteLesson);
+router.delete(
+  "/delete-lesson/:lessonId",
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  LessonControllers.deleteLesson,
+);
 
 export const LessonRoutes = router;
