de3ff3eff81ad3d2d9077a6f3687a2fd24c6f219
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2024-11-26 17:05:16 +0600

    update topic route auth

--- src/app/modules/topic/topic.route.ts
+++ src/app/modules/topic/topic.route.ts
@@ -2,11 +2,14 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { TopicControllers } from "./topic.controller";
 import { TopicValidations } from "./topic.validation";
+import { USER_ROLE } from "../user/user.constant";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post(
   "/create-topic",
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(TopicValidations.createTopicValidationSchema),
   TopicControllers.createTopic,
 );
@@ -16,16 +19,21 @@ router.get("/", TopicControllers.getAllTopics);
 
 router.patch(
   "/update-topic/:topicId",
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(TopicValidations.updateTopicValidationSchema),
   TopicControllers.updateTopic,
 );
 
-router.post(
-  "/link-lesson",
-  validateRequest(TopicValidations.linkLessonToTopicValidationSchema),
-  TopicControllers.linkLessonToTopic,
-);
+// router.post(
+//   "/link-lesson",
+//   validateRequest(TopicValidations.linkLessonToTopicValidationSchema),
+//   TopicControllers.linkLessonToTopic,
+// );
 
-router.delete("/delete-topic/:topicId", TopicControllers.deleteTopic);
+router.delete(
+  "/delete-topic/:topicId",
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  TopicControllers.deleteTopic,
+);
 
 export const TopicRoutes = router;
