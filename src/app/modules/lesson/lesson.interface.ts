0f4304a57ac7f8199694cc1de7a743ecbf725da6
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2025-01-15 15:22:28 +0600

    add lesson materials in lesson

--- src/app/modules/lesson/lesson.interface.ts
+++ src/app/modules/lesson/lesson.interface.ts
@@ -5,5 +5,11 @@ export type TLesson = {
   type: "video" | "assignment" | "quiz" | "post";
   isCompleted: boolean;
   completedAt: Date;
+  materials: [
+    {
+      name: string;
+      link: string;
+    },
+  ];
   isDeleted: boolean;
 };
